e9aa942457e9833a5997e56f34a44f3fbc247641
Pastew <kpastew@gmail.com>
2017-11-06 10:24:50 +0100

    Larger window

--- desktop/src/com/pastew/plague/desktop/DesktopLauncher.java
+++ desktop/src/com/pastew/plague/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.pastew.plague.Plague;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 700;
-		config.height = 700;
+		config.width = 1280;
+		config.height = 960;
 		new LwjglApplication(new Plague(), config);
 	}
 }
