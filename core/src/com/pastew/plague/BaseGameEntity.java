f4939db1b9f15a4a9411a1edd06ebd1fa7223e75
Pastew <kpastew@gmail.com>
2017-12-17 21:41:08 +0100

    tagneighbors probably works

--- core/src/com/pastew/plague/BaseGameEntity.java
+++ core/src/com/pastew/plague/BaseGameEntity.java
@@ -3,6 +3,10 @@ package com.pastew.plague;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import java.util.List;
+
+import javax.swing.text.html.parser.Entity;
+
 public class BaseGameEntity {
 
     Vector2D position;
@@ -10,6 +14,7 @@ public class BaseGameEntity {
     ShapeRenderer shapeRenderer;
     protected float size = 20;
 
+    boolean tag = false;
 
     BaseGameEntity() {
         position = new Vector2D(0, 0);
@@ -25,5 +30,25 @@ public class BaseGameEntity {
         shapeRenderer.setColor(color);
         shapeRenderer.circle((float) position.x, (float) position.y, size / 2);
         shapeRenderer.end();
+    }
+
+    void tagNeighbors(BaseGameEntity entity, List<BaseGameEntity> entities, double radius){
+        for(BaseGameEntity currentEntity : entities){
+            currentEntity.unTag();
+
+            Vector2D to = Vector2DOperations.sub(currentEntity.position, entity.position);
+            double range = radius + currentEntity.size/2;
+            if(currentEntity != entity && to.LengthSq() < range * range){
+                currentEntity.tag();
+            }
+        }
+    }
+
+    private void tag() {
+        tag = false;
+    }
+
+    private void unTag() {
+        tag = false;
     }
 }
